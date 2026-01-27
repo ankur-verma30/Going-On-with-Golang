1bb2d428845f7476faed508234278e8ded8e232e
Ankur Verma <ankurverma7619@gmail.com>
2026-01-28 01:14:18 +0530

    Learned variables in GO

--- Exploring-Variables-and-Constants/main.go
+++ Exploring-Variables-and-Constants/main.go
@@ -1,7 +1,39 @@
 package main
 
 import "fmt"
+var myInt int=10
 
+
+//grouping variable: Used to group similar variables
+var(
+	myString="Hello"
+	myInt2=20
+)
 func main(){
-	
+	// variable in golang
+	fmt.Println(myInt)
+	doSomething()
+
+	//group variables can be written inside the function as well -> rarely used
+	var(
+		myBool bool=true
+		myFloat float64=10.5
+	)
+
+	fmt.Println("Outside the main method group variables ",myString)
+	fmt.Println("Outside the main method group variables ",myInt2)
+	fmt.Println("Inside the main method group variables ",myBool)
+	fmt.Println("Inside the main method group variables ",myFloat)
+
+	//using short hand notation ----------> only used inside the function
+	a:=10
+	b:=20
+	fmt.Println("These are shorthand variables ",a,b)
+	// Atmost one new variable shoule be declared on the left side for redeclaration of pre-existing variables
+	c,a:=10,20
+	fmt.Println("These are shorthand variables with one new variables and other pre-existing variables ",c,a)
+}
+
+func doSomething(){
+	fmt.Println("Do something: ",myInt)
 }
