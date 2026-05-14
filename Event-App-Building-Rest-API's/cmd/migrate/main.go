16568a6cdc84ea75978295ae98482ce429f84a78
Ankur Verma <ankurverma7619@gmail.com>
2026-05-14 23:51:51 +0530

    Working on database connection with the api

--- Event-App-Building-Rest-API's/cmd/migrate/main.go
+++ Event-App-Building-Rest-API's/cmd/migrate/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"database/sql"
-	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"log"
 	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/sqlite3"
+	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
+	"github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 func main() {
